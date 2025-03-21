79554b09b8f3a473151572d8a6d742c5b2ebd276
ClaudS2006 <165497828+ClaudS2006@users.noreply.github.com>
2025-03-21 17:09:22 +0100

    api change with date & time
    get new duck pic every time

--- PRO1001 WorkRequ5.js
+++ PRO1001 WorkRequ5.js
@@ -1,12 +1,16 @@
 // https://corsproxy.io/?https://random-d.uk/api/v2/random 
+// https://api.allorigins.win/raw?url=https://random-d.uk/api/v2/random
+// used 2nd api for testing
 
 const header = document.getElementById("entry-sign");
 const para = document.getElementById("profile-pic");
 const btn = document.getElementById("happy-duck");
+
 // function to get picture-----------------------------------
 async function fetchData(){
     try{
-        const randomPic = await fetch("https://cors-anywhere.herokuapp.com/https://random-d.uk/api/v2/random ");
+        // to avoid cache I added new date/time to make it look like new request
+        const randomPic = await fetch("https://api.allorigins.win/raw?url=https://random-d.uk/api/v2/random?time="+ new Date().getTime());
         if(!randomPic.ok) throw new Error(`Fetch Error: ${randomPic.status}`);
         const duckPic = await randomPic.json();
 
@@ -29,6 +33,8 @@ btn.addEventListener("click", async () => {
 
     const data = await fetchData();
     await new Promise(resolve => setTimeout(resolve,1000));
+
+    para.textContent = "";
     
     if(data.duckPic){
         const img = document.createElement("img");
